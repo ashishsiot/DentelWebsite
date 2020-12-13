0485e49b9deed2623c92bbfd273569afc8791a24
Ved Patel <67530938+25ved10@users.noreply.github.com>
2020-12-13 21:36:27 +0530

    done

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 let header =document.querySelector('.header');
 let hamburgerMenu =document.querySelector('.hamburger-menu');
 
+window.addEventListener('scroll',function(){
+    let windowPosition = window.scrollY > 300;      //changes title bar back to solid 
+    header.classList.toggle('active', windowPosition)
+})
+
 hamburgerMenu.addEventListener('click', function() {
     header.classList.toggle('menu-open');
 })
